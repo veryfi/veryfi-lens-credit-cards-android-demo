f8540154fb7e2e511edd0de0eeea8d5150d54b95
Alejandro Uribe Sánchez <alejouribesanchez@gmail.com>
2022-04-06 00:10:08 -0500

    Update settings.gradle

--- LensCreditCardsDemo/app/src/main/java/com/veryfi/lens/credit/cards/demo/Application.kt
+++ LensCreditCardsDemo/app/src/main/java/com/veryfi/lens/credit/cards/demo/Application.kt
@@ -9,10 +9,10 @@ class Application: Application() {
 
     companion object {
         //REPLACE YOUR KEYS HERE
-        const val CLIENT_ID = "xxx"
-        const val AUTH_USERNAME = "xxx"
-        const val AUTH_API_KEY = "xxx"
-        const val URL = "https://xxx.xxx"
+        const val CLIENT_ID = BuildConfig.VERYFI_CLIENT_ID
+        const val AUTH_USERNAME = BuildConfig.VERYFI_USERNAME
+        const val AUTH_API_KEY = BuildConfig.VERYFI_API_KEY
+        const val URL = BuildConfig.VERYFI_URL
     }
 
     override fun onCreate() {
@@ -29,7 +29,7 @@ class Application: Application() {
         veryfiLensSettings.autoCaptureIsOn = true
         veryfiLensSettings.autoRotateIsOn = true
         veryfiLensSettings.autoSubmitDocumentOnCapture = true
-        veryfiLensSettings.documentTypes = arrayListOf("receipt")
+        veryfiLensSettings.documentTypes = arrayListOf("credit_card")
         veryfiLensSettings.galleryIsOn = false
         veryfiLensSettings.moreMenuIsOn = false
         veryfiLensSettings.showDocumentTypes = true
