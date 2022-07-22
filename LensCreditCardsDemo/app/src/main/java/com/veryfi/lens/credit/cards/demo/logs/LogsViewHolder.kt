19c3ebf37787db5dbe12ad77dce2e9d52982995b
Bernardo Restrepo <bernardorestrepo@veryfi.com>
2022-07-22 09:06:21 -0500

    Add settings UI

---
+++ LensCreditCardsDemo/app/src/main/java/com/veryfi/lens/credit/cards/demo/logs/LogsViewHolder.kt
@@ -0,0 +1,13 @@
+package com.veryfi.lens.credit.cards.demo.logs
+
+import androidx.recyclerview.widget.RecyclerView
+import com.veryfi.lens.credit.cards.demo.databinding.LogsListItemBinding
+
+class LogsViewHolder(private val itemBinding: LogsListItemBinding) :
+    RecyclerView.ViewHolder(itemBinding.root) {
+
+    fun onBind(log: Log) {
+        itemBinding.logsTitle.text = log.title
+        itemBinding.logsMessage.text = log.message
+    }
+}
