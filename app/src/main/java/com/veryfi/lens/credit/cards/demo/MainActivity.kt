2a3033bc067ca605fce82e5f2f0bb1113be157a0
Alejandro Maldonado <alejandromaldonado@veryfi.com>
2026-08-06 13:11:27 -0500

    ANDLENS-1642: Updated Android Lens to 2.6.0.15

--- app/src/main/java/com/veryfi/lens/credit/cards/demo/MainActivity.kt
+++ app/src/main/java/com/veryfi/lens/credit/cards/demo/MainActivity.kt
@@ -72,7 +72,6 @@ class MainActivity : AppCompatActivity() {
     private var boostModeIsOn = veryfiLensSettings.boostModeIsOn
     private var boundingBoxesIsOn = veryfiLensSettings.boundingBoxesIsOn
     private var detectBlurResponseIsOn = veryfiLensSettings.detectBlurResponseIsOn
-    private var isProduction = veryfiLensSettings.isProduction
     private var confidenceDetailsIsOn = veryfiLensSettings.confidenceDetailsIsOn
     private var parseAddressIsOn = veryfiLensSettings.parseAddressIsOn
     private var externalId = veryfiLensSettings.externalId ?: ""
@@ -120,7 +119,6 @@ class MainActivity : AppCompatActivity() {
         viewBinding.switchBoostMode.isChecked = boostModeIsOn
         viewBinding.switchBoundingBoxes.isChecked = boundingBoxesIsOn
         viewBinding.switchDetectBlurResponse.isChecked = detectBlurResponseIsOn
-        viewBinding.switchIsProduction.isChecked = isProduction
         viewBinding.switchConfidenceDetails.isChecked = confidenceDetailsIsOn
         viewBinding.switchParseAddress.isChecked = parseAddressIsOn
         viewBinding.switchIgnoreRemoteSettings.isChecked = ignoreRemoteSettings
@@ -215,10 +213,6 @@ class MainActivity : AppCompatActivity() {
 
         viewBinding.switchDetectBlurResponse.setOnCheckedChangeListener { _, isChecked ->
             detectBlurResponseIsOn = isChecked
-        }
-
-        viewBinding.switchIsProduction.setOnCheckedChangeListener { _, isChecked ->
-            isProduction = isChecked
         }
 
         viewBinding.switchConfidenceDetails.setOnCheckedChangeListener { _, isChecked ->
@@ -442,7 +436,6 @@ class MainActivity : AppCompatActivity() {
         veryfiLensSettings.boostModeIsOn = boostModeIsOn
         veryfiLensSettings.boundingBoxesIsOn = boundingBoxesIsOn
         veryfiLensSettings.detectBlurResponseIsOn = detectBlurResponseIsOn
-        veryfiLensSettings.isProduction = isProduction
         veryfiLensSettings.confidenceDetailsIsOn = confidenceDetailsIsOn
         veryfiLensSettings.parseAddressIsOn = parseAddressIsOn
         veryfiLensSettings.gpuIsOn = gpuIsOn
